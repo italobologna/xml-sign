13c0b9ba52dcbc8d8c9d64e384175572573d853f
Italo <italoviske@hotmail.com>
2020-09-17 21:46:32 -0300

    Changed libraries for canonicalizing the tags

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-var libxmljs = require("libxmljs");
+const DOMParser = require('xmldom').DOMParser;
+const xpath = require('xpath');
+const c14n = require("xml-c14n")();
 
-function signXml(xml, options) {
-  return 'Hello World';
+function signXml(xml, options, cb) {
+  try {
+
+    // Transform the XMl into a document object
+    let doc = new DOMParser().parseFromString(xml);
+
+    // Gets the specified value from the object
+    // Needs a for loop
+    let toSign = xpath.select1(options.toSign[0], doc);
+    {
+      // toSign element to string
+      console.log(toSign.toString());
+
+      var canonicaliser = c14n
+          .createCanonicaliser("http://www.w3.org/2001/10/xml-exc-c14n#");
+      // .createCanonicaliser("http://www.w3.org/2001/10/xml-exc-c14n#WithComments");
+      canonicaliser.canonicalise(toSign,
+          function (err, res) {
+            if (err) {
+              console.warn(err.stack);
+              cb(err);
+              return;
+            }
+
+
+
+
+
+            cb(null, res);
+          });
+    }
+  } catch (e) {
+    cb(e);
+  }
 }
 
 module.exports = signXml;
--- test/index.test.js
+++ test/index.test.js
@@ -6,10 +6,15 @@ describe('Testing', function () {
   let xml = fs.readFileSync('./test/resources/pacs008.xml').toString();
 
   it('Can do things', function () {
-    console.log(xml);
-    console.log(signXml());
+    // console.log(xml);
+    console.log(signXml(xml, {
+      toSign: [
+        '//*[local-name(.)=\'AppHdr\']',
+        '//*[local-name(.)=\'Document\']'
+      ]
+    }, function(err, res) {
+      console.log(err);
+      console.log(res);
+    }));
   });
 });
-
-
-
