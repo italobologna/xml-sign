2cd687809508f8792f5f85f3cf0963ac658c5d7b
Italo <italoviske@hotmail.com>
2020-09-17 21:46:48 -0300

    added the key info method to generate the key info according to the specification

---
+++ test/keyinfo.test.js
@@ -0,0 +1,16 @@
+const keyInfo = require('./../src/keyinfo');
+const assert = require('assert');
+
+describe('key info tag generation tests', function () {
+
+  const keyId = 'a1';
+  const issuerName = 'issuerName'
+  const hexSerialNumber = '0123456789ABCDEFFEDCBA9876543210'
+
+  it('Should generate the proper key info with information', function () {
+    let res = keyInfo(keyId, issuerName, hexSerialNumber);
+    console.log(res);
+    assert.deepStrictEqual(res,
+        `<KeyInfo Id=${ keyId }><X509Data><X509IssuerSerial><X509IssuerName>${ issuerName }</X509IssuerName><X509SerialNumber>${ hexSerialNumber }</X509SerialNumber></X509IssuerSerial></X509Data></KeyInfo>`);
+  });
+});
