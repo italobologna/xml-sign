cee908b5bf3e24f4e1823456a9340dbc3d145f56
Italo <italoviske@hotmail.com>
2020-09-20 14:12:06 -0300

    Adds the a test for the key info certificate information extractor.

---
+++ test/main/createkeyinfo.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const fs = require('fs');
+const createKeyInfo = require("../../src/main/createkeyinfo");
+
+describe('test extract information from certificate', function () {
+
+  let cert = fs.readFileSync('./test/resources/cert.pem').toString();
+
+  it('can generate information for the key info tag', function () {
+    let extractedInformation = createKeyInfo(cert);
+    assert.deepStrictEqual(extractedInformation.issuerName, 'C=BR, ST=SAOPAULO, L=Osasco, O=Avengers, OU=PTT, CN=Italo, E=email@email.com');
+    assert.deepStrictEqual(extractedInformation.keyId, '4e5b2c37cf49b836a3d21077247e27862f9db380');
+    assert.deepStrictEqual(extractedInformation.serialNumber, '6e31836d5e322ae0b6c9e053720e5b85addf8928');
+  });
+});
